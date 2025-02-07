40414bc46b7bc5edadb4cb900c80426034cac8f7
Siva Prakash <prakashdeveloper@outlook.in>
2025-02-07 15:44:26 +0530

    Updated format.sh

--- format.sh
+++ format.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-# Check if clang-format is installed
-if command -v clang-format >/dev/null 2>&1; then
-  echo "clang-format is already installed."
+# Check if google-java-format JAR is already downloaded
+if [ -f google-java-format.jar ]; then
+  echo -e "\ngoogle-java-format JAR already downloaded."
 else
-  echo "Installing clang-format..."
-  sudo apt-get update
-  sudo apt-get install -y clang-format
+  echo -e "\nDownloading google-java-format JAR..."
+  curl -L -o google-java-format.jar https://github.com/google/google-java-format/releases/download/v1.26.0/google-java-format-1.26.0-all-deps.jar
   if [ $? -ne 0 ]; then
-    echo "Error: Failed to install clang-format."
+    echo "Error: Failed to download google-java-format JAR."
     exit 1
   fi
 fi
 
-# Format C++ files
-echo -e "\nFormatting C++ files..."
-find . -type f -name "*.cpp" | while read -r file; do
+# Format Java files
+echo -e "\nFormatting Java files..."
+find . -type f -name "*.java" | while read -r file; do
   echo "Formatting $file..."
-  clang-format -style=llvm -i "$file"
+  java -jar google-java-format.jar --replace "$file"
   if [ $? -ne 0 ]; then
     echo "Error: Failed to format $file."
     exit 1
